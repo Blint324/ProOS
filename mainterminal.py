307bbf3e1e08c31f4b0a6df882afd7aad2807cd6
Blint324 <161594404+Blint324@users.noreply.github.com>
2025-07-09 18:25:23 +0200

    Added the mainterminal.py file.

---
+++ mainterminal.py
@@ -0,0 +1,42 @@
+print("Welcome to ProOS!")
+print("Importing core libraries...")
+
+import os
+import time
+import datetime
+import random
+
+print("Defining variables...")
+
+cmd = ""
+cwd = os.getcwd()
+
+print("Defining variables done.")
+print("Starting main loop...")
+
+while True:
+    cmdSep = []
+    cmd = input(f"{cwd}: ")
+    cmd.lower()
+
+    # Checking for commands
+    
+    if cmd[0:3] == "ver":
+        if len(cmd) == 3:
+            print("ProOS version u1.0")
+        else:
+            if cmd[3:6] == " -d":
+                print("ProOS version u1.0\nCredits:\n Bálint Vámosi: Lead developer.\n Linus Torvalds and the Linux team: Inspiration for this mockup.")
+    
+            elif cmd[3:6] == " -h":
+                print("Displays the current version of ProOS.\nUsage:\nver [FLAGS]")
+    elif cmd[0:4] == "help":
+        if len(cmd) == 4:
+            print("Invalid usage!\nUsage:\nhelp [COMMAND]")
+        else:
+            if cmd[5:8] == "ver":
+                print("Prints the current version of ProOS.\nFlags:\n-d: Displays more detailed info about ProOS.\n-h: Displays help for the command.")
+            else:
+                print(f"No help document detected for {cmd[5:999999999999999999999999999999999999999]}.")
+    elif cmd[0:4] == "exit":
+        quit(0)
