b761412d42111883e92e9f0abb92f1d4e6bee7a3
Blint324 <blint324gd@gmail.com>
2025-07-09 21:56:54 +0200

    VERSION u1.1 WOOOOOOO

--- mainterminal.py
+++ mainterminal.py
@@ -8,6 +8,7 @@ print("Importing core libraries...")
 import time
 import datetime
 import random
+import linecache
 
 print("Defining variables...")
 
@@ -25,16 +26,16 @@ while True:
     
     if cmd[0:3] == "ver":
         if len(cmd) == 3:
-            print("ProOS version u1.0")
+            print("ProOS version u1.1")
         else:
             if cmd[3:6] == " -d":
-                print("ProOS version u1.0\nCredits:\n Bálint Vámosi: Lead developer.\nLinus Torvalds and the Linux team:Inspiration for this mockup.")
+                print("ProOS version u1.0\nCredits:\nBálint Vámosi: Lead developer.\nLinus Torvalds and the Linux team: Inspiration for this mockup.\nGergő Vámosi: Co-Owner of the project.")
     
             elif cmd[3:6] == " -h":
                 print("Displays the current version of ProOS.\nUsage:\nver [FLAGS]")
     elif cmd[0:4] == "help":
         if len(cmd) == 4:
-            print("Basic commands:\nrepeat\nexit\nver.")
+            print("Basic commands:\nrepeat\nexit\nver\ndisplay.")
         else:
             if cmd[5:8] == "ver":
                 print("Prints the current version of ProOS.\nFlags:\n-d: Displays more detailed info about ProOS.\n-h: Displays help for the command.")
@@ -42,6 +43,10 @@ while True:
                 print("Exits the program.")
             elif cmd[5:11] == "repeat":
                 print("Repeats whatever you put next to it.")
+            elif cmd[5:12] == "display":
+                print("Displays the contents of any text file.\nFlags:\n-su: Runs as superuser.")
+            elif cmd[5:10] == "clear":
+                print("Clears the contents of the screen.")
             else:
                 print(f"No help document detected for {cmd[5:999999999999999999999999999999999999999]}.")
     elif cmd[0:4] == "exit":
@@ -50,4 +55,16 @@ while True:
         if len(cmd) == 6:
             print("Invalid usage!\nUsage:\nrepeat [ANYTHING]")
         else:
-            print(cmd[6:9999999999999999999999999999999999999999999999999])
+            print(cmd[7:9999999999999999999999999999999999999999999999999])
+    elif cmd[0:7] == "display":
+        if len(cmd) == 7:
+            os.system(f"cat {cmd[8:999999999999999999999999999999999999]}")
+        else:
+            if cmd[8:11] == "-su":
+                os.system(f"sudo cat {cmd[8:99999999999999999999999999999999]}")
+            else:
+                print(f"Flag {cmd[8:9999999999999999999999999999999999999999999999999999]} not found!")
+    elif cmd[0:5] == "clear":
+        os.system("clear")
+    else:
+        print(f"Command {cmd} not found!")
